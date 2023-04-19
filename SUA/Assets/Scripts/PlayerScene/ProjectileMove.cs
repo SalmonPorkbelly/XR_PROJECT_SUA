bb15a634fc4415784b1e158801e144c44384ecef
suakim <salmonporkbelly@gmail.com>
2023-04-19 22:39:50 +0900

    add monstercontroller

--- SUA/Assets/Scripts/PlayerScene/ProjectileMove.cs
+++ SUA/Assets/Scripts/PlayerScene/ProjectileMove.cs
@@ -4,33 +4,46 @@ using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
+    public enum PROJECTILETYPE
+    {
+        PLAYER,
+        MONSTER
+    }
     public Vector3 launchDirection; // 발사방향
+
+    public PROJECTILETYPE projectileType;
+
 
     private void FixedUpdate() {
         float moveAmount = 3 * Time.fixedDeltaTime; // 이동 속도 설정
         transform.Translate(launchDirection * moveAmount); // translate로 이동
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.tag == "Object") // Tag 값이 오브젝트인 경우
-        {
-            Destroy(this.gameObject);
-        }
-        if (collision.gameObject.tag == "Monster") // Tag 값이 Monster인 경우
-        {
-            Destroy(this.gameObject);
-            collision.gameObject.GetComponent<Monster>().Damaged(1);
-        }
-
-    }
+    // private void OnCollisionEnter(Collision collision) {
+    //     Debug.Log(collision.gameObject.name);
+    //     if (collision.gameObject.tag == "Object") // Tag 값이 오브젝트인 경우
+    //     {
+    //         Destroy(this.gameObject);
+    //     }
+    //     if (collision.gameObject.tag == "Monster") // Tag 값이 Monster인 경우
+    //     {
+    //         Destroy(this.gameObject);
+    //         collision.gameObject.GetComponent<Monster>().Damaged(1);
+    //     }
+    // }
 
     private void OnTriggerEnter(Collider other) 
     {
-        if(other.CompareTag("Monster"))
+        if(other.CompareTag("Monster") && projectileType == PROJECTILETYPE.PLAYER) //Tag를 검사한다.
         {
             Destroy(this.gameObject);
             other.gameObject.GetComponent<Monster>().Damaged(1);
+        }
+
+        if(other.CompareTag("Player") && projectileType == PROJECTILETYPE.MONSTER) //Tag를 검사한다.
+        {
+            Destroy(this.gameObject);
+            other.gameObject.GetComponent<PlayerHp>().Damaged(1);
         }
     }
 }
