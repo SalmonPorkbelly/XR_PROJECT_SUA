c6d39d4f40cdcc0e3fa957cc4bb5e76dd416479e
Sua Kim <salmonporkbelly@gmail.com>
2023-06-23 23:02:47 +0900

    commit

--- SUA/Assets/Scripts/PlayerScene/GameManager.cs
+++ SUA/Assets/Scripts/PlayerScene/GameManager.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //UI추가
+using System;
 
 
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameState //게임 상태값 설정
+    {
+        Start,
+        Playing,
+        GameOver
+    }
+
+    public event Action<GameState> OnGameStateChanged;
+
+    public GameState currentState = GameState.Start;
+
+    public GameState CurrentState
+    {
+        get { return currentState; }
+        private set
+        {
+            currentState = value;
+            OnGameStateChanged?.Invoke(currentState); //이벤트가 null이 아닌 경우에만 이 이벤트를 호출
+        }
+    }
+
+    public void StartGame()
+    {
+        //게임 시작 로직을 여기에 작성
+        CurrentState = GameState.Playing;
+    }
+
+    public void GameOver()
+    {
+        //게임 오버 로직을 여기에 작성
+        CurrentState = GameState.GameOver;
+    }
+
+
+
     public GameManager() {}
     public static GameManager Instance {get; private set;} // 싱글톤화
     public PlayerHp playerHp; // 플레이어의 Hp
     public Image playerHpUIImage; //플레이어의 Hp UI 이미지
     public Button BtnSample;
 
-    private void Start() {
-        this.BtnSample.onClick.AddListener(()=> //Linstener로 버튼 등록
-        {
-            Debug.Log("Button Check");
-        });
-    }
     
     private void Awake()
     {
